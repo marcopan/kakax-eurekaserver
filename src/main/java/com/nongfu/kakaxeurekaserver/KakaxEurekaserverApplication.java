199a35109dd4e67f45c26afd2a8ad674c344e2ff
marcopan <370510052@qq.com>
2021-01-30 13:43:09 +0800

    first commit

---
+++ src/main/java/com/nongfu/kakaxeurekaserver/KakaxEurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.nongfu.kakaxeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class KakaxEurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaxEurekaserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nongfu/kakaxeurekaserver/KakaxEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nongfu.kakaxeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaxEurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
